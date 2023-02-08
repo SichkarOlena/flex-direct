f085d35c8e6e864f7f98cbdcf4e6f1761e85e1df
Alena Semivolos <semivolos2105@gmail.com>
2023-02-08 12:56:05 +0200

    init

---
+++ flex.js
@@ -0,0 +1,15 @@
+const rowChange = () => {
+    document.getElementById("box").style.flexDirection = "row";
+}
+
+const rowReverseChange = () => {
+    document.getElementById("box").style.flexDirection = "row-reverse";
+}
+
+const columnChange = () => {
+    document.getElementById("box").style.flexDirection = "column";
+}
+
+const columnReverseChange = () => {
+    document.getElementById("box").style.flexDirection = "column-reverse"
+};
